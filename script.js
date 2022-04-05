ec7853bfb0adb18690cc4405fa6551f9b214d2e1
dmasdi123 <febriandimas634@gmail.com>
2022-04-06 03:24:26 +0700

    menambahkan method penumpangNaik dan penumpangTurun

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ function Bison(sopir, trayek, penumpang, kas) {
   this.trayek = trayek;
   this.penumpang = penumpang;
   this.kas = kas;
+
+  this.penumpangNaik = function (namaPenumpang) {
+    this.penumpang.push(namaPenumpang);
+    return this.penumpang;
+  };
+
+  this.penumpangTurun = function (namaPenumpang, bayar) {
+    if (this.penumpang.length == 0) {
+      alert("Bison Masih Kosong");
+      return false;
+    }
+
+    for (var i = 0; i < this.penumpang.length; i++) {
+      if (this.penumpang[i] == namaPenumpang) {
+        this.penumpang[i] == undefined;
+        this.kas += bayar;
+        return this.penumpang;
+      }
+    }
+  };
 }
 
 var bison1 = new Bison("Cak Idris", ["Surabaya", "Sidoarjo"], [], 0);
